6671fe64276ae4d24e88093b433dfbd7f0aca7f0
rod101 <rodney.addo@gmail.com>
2020-05-20 20:22:34 +0200

    created form component

--- index.js
+++ index.js
@@ -1,65 +1,107 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
-	const testData = [
-			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-  		{name: "johnpapa", avatar_url: "https://avatars2.githubusercontent.com/u/1202528?v=4", company: "JohnPapa.net, LLC"},
-	];
+// Test Data
 
+const testData = [
+  {
+    name: "Dan Abramov",
+    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+    company: "@facebook"
+  },
+  {
+    name: "Sophie Alpert",
+    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+    company: "Humu"
+  },
+  {
+    name: "Sebastian Markbåge",
+    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+    company: "Facebook"
+  },
+  {
+    name: "johnpapa",
+    avatar_url: "https://avatars2.githubusercontent.com/u/1202528?v=4",
+    company: "JohnPapa.net, LLC"
+  }
+];
 
-const CardList = (props) => (
+const CardList = props => (
   <div>
-
-  {/* this is the simple way todo it
+    {/* this is the simple way todo it
    <Card {...testData[0]} />
   <Card {...testData[1]} />
   */}
-{/*Cleaning up code, rewrite */}
-{testData.map(profile => <Card {...profile}/>)}
-
-
+    {/*Cleaning up code, rewrite */}
+    {props.profile.map(profile => (
+      <Card {...profile} />
+    ))}
   </div>
-)
+);
 
 class Card extends React.Component {
-  render(){
+  render() {
     const profile = this.props;
-    return (  
+    return (
       <div className="github-profile">
-      <img src={profile.avatar_url} />
-      <div className="info">
-    <div className="name">{profile.name}</div>
-    <div className="company">{profile.company}</div>
-      </div>
+        <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
       </div>
     );
   }
 }
 
+class Form extends React.Component {
+  // userNameInput = React.createRef(); // no more useing ref
+  state = {
+    userName: ''
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.userName);
+
+    // using ref
+  //   console.log(this.userNameInput.current.value);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={event => this.setState({userName: event.target.value})}
+          placeholder="Github username"
+          //ref={this.userNameInput} // there is a better way to do this
+          required
+        />
+        <button>Add Card</button>
+      </form>
+    );
+  }
+}
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      name: 'React'
+      profiles: testData
     };
   }
 
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-        <CardList />
+        <h1>The GitHub Card App</h1>
+        <Form />
+        <CardList profile={this.state.profiles} />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
